738a7355dac5b2c860bd5c1eaab339ef0a24d24f
anandraina21 <anandraina95@gmail.com>
2020-03-03 00:46:05 +0530

    Changes in UI and code structure

--- app/src/main/java/com/example/calculatordemo/MainActivity.kt
+++ app/src/main/java/com/example/calculatordemo/MainActivity.kt
@@ -1,5 +1,8 @@
 /**
+ * Simple Calculator app developed using Kotlin
  * @author : Anand Raina
+ * @version : 1.0
+ * @since : 1.0
  * */
 
 
@@ -11,69 +14,112 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    var num1 : Double = 0.0
-    var num2 : Double = 0.0
+    // TODO : Add variables and helper methods below
+    var numValue : String = ""
+    var num : Double = 0.0
+    var temp : Double = 0.0
 
-    private fun checkIllegalValues() {
-        if (editText_enter_number_1.text.toString().equals(null) || editText_enter_number_1.text.toString().equals("")
-            || editText_enter_number_1.text.toString().equals(".")
-        ) {
-            editText_enter_number_1.setText("0")
-        }
-
-        if (editText_enter_number_2.text.toString().equals(null) || editText_enter_number_2.text.toString().equals("")
-            || editText_enter_number_2.text.toString().equals(".")
-        ) {
-            editText_enter_number_2.setText("0")
+    private fun insertNum(numValue : String){
+        if(editText.text.toString().equals("0")) {
+            editText.setText(numValue)
+        } else {
+            editText.setText(editText.text.toString() + numValue)
         }
     }
-    
-    private fun setValues() {
-        num1 = editText_enter_number_1.text.toString().toDouble()
-        num2 = editText_enter_number_2.text.toString().toDouble()
-    }
-    
-    private fun showResult(result: Double) {
-        var output: String = result.toString()
-        editText_show_result.setText(output)
-    }
 
+    // TODO : Add activity specific tasks below
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // TODO : Add code below
         button_clear.setOnClickListener {
-            editText_enter_number_1.setText("0")
-            editText_enter_number_2.setText("0")
-            editText_show_result.setText("")
+            editText.setText("0")
+        }
+        button_add.setOnClickListener{
+            num = editText.text.toString().toDouble()
+            editText.setText(editText.text.toString() + " + ")
+
+        }
+        button_subtract.setOnClickListener{
+            num = editText.text.toString().toDouble()
+            editText.setText(editText.text.toString() + " - ")
+        }
+        button_multiply.setOnClickListener{
+            num = editText.text.toString().toDouble()
+            editText.setText(editText.text.toString() + " * ")
+        }
+        button_divide.setOnClickListener{
+            num = editText.text.toString().toDouble()
+            editText.setText(editText.text.toString() + " / ")
+        }
+        button_result.setOnClickListener{
+            var num2 : String = editText.text.toString()
+            var tempString : String = ""
+            var calcOperator : String = ""
+            var i : Int = num2.length - 1
+            while(i>=0) {
+                if(num2[i].equals(' ')) {
+                    calcOperator = num2[i-1].toString()
+                    break
+                } else {
+                    tempString = num2[i] + tempString
+                    i--
+                }
+            }
+            temp = tempString.toDouble()
+            if(calcOperator.equals("+")) {
+                num = num + temp
+            } else if(calcOperator.equals("-")) {
+                num = num - temp
+            } else if(calcOperator.equals("*")) {
+                num = num * temp
+            } else if(calcOperator.equals("/")) {
+                num = num / temp
+            }
+            var finalResult : String = num.toString()
+            editText.setText(finalResult)
         }
 
-        button_add.setOnClickListener {
-            checkIllegalValues()
-            setValues()
-            var result: Double = num1 + num2
-            showResult(result)
+        button1.setOnClickListener {
+            numValue = "1"
+            insertNum(numValue)
         }
-
-        button_subtract.setOnClickListener {
-            checkIllegalValues()
-            setValues()
-            var result: Double = num1 - num2
-            showResult(result)
+        button2.setOnClickListener {
+            numValue = "2"
+            insertNum(numValue)
         }
-
-        button_multiply.setOnClickListener {
-            checkIllegalValues()
-            setValues()
-            var result: Double = num1 * num2
-            showResult(result)
+        button3.setOnClickListener {
+            numValue = "3"
+            insertNum(numValue)
         }
-
-        button_divide.setOnClickListener {
-            checkIllegalValues()
-            setValues()
-            var result: Double = num1 / num2
-            showResult(result)
+        button4.setOnClickListener {
+            numValue = "4"
+            insertNum(numValue)
+        }
+        button5.setOnClickListener {
+            numValue = "5"
+            insertNum(numValue)
+        }
+        button6.setOnClickListener {
+            numValue = "6"
+            insertNum(numValue)
+        }
+        button7.setOnClickListener {
+            numValue = "7"
+            insertNum(numValue)
+        }
+        button8.setOnClickListener {
+            numValue = "8"
+            insertNum(numValue)
+        }
+        button9.setOnClickListener {
+            numValue = "9"
+            insertNum(numValue)
+        }
+        button0.setOnClickListener {
+            numValue = "0"
+            insertNum(numValue)
         }
     }
 }
